9ae67a721eb36933ec2ac95f3f72d267017b6861
lihongtao@17leyou.com <lihongtao@17leyou.com>
2013-07-26 10:01:06 +0800

    upyun

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.storage = :upyun
-  config.upyun_username = "hongtaolee"
-  config.upyun_password = 'shuang'
-  config.upyun_bucket = "tutu-test"
-  config.upyun_bucket_domain = "tutu-test.b0.upaiyun.com"
+  config.upyun_username = "yuyuanwai"
+  config.upyun_password = '1q2w3e4r'
+  config.upyun_bucket = "kaisa-images"
+  config.upyun_bucket_domain = "kaisa-images.b0.upaiyun.com"
 end
