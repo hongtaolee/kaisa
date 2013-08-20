b9682a3aba3ded72a10566bef5d91e047bd55a00
lihongtao@17leyou.com <lihongtao@17leyou.com>
2013-08-20 15:32:33 +0800

    add upyun

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,5 +3,5 @@ CarrierWave.configure do |config|
   config.upyun_username = "yuyuanwai"
   config.upyun_password = '1q2w3e4r'
   config.upyun_bucket = "kaisa-images"
-  config.upyun_bucket_domain = "kaisa-images.b0.upaiyun.com"
+  config.upyun_bucket_domain = "p1.kaisa029.com"
 end
